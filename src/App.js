dffc7b7c8b54504ceca908fd68710424b75089ef
Sondinh <dnson20022001@gmail.com>
2022-04-30 11:30:51 +0700

    commit code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 
+import SideBar from './components/SideBar/SideBar';
+import Logo from './components/Logo/Logo';
+import Paragraph from './components/Paragraph/Paragraph';
+import ItemList from './components/ItemList/ItemList';
+import Footer from './components/Footer/Footer';
+
 function App() {
     return (
         <div className="App">
-            <p>Hello World</p>
+            <SideBar />
+
+            <div className="container">
+                <div style={{ padding: '0 10%' }}>
+                    <Logo />
+                    <Paragraph />
+                    <ItemList />
+                </div>
+                <Footer />
+            </div>
         </div>
     );
 }
